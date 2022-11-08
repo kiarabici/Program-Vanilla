89f77f2f401b0cded8e1e95d03e30f154f165a5c
Kiara Bici <kiara.bici16@gmail.com>
2022-11-08 15:17:06 +0100

    Program in Vanilla

---
+++ start.js
@@ -0,0 +1,60 @@
+const http = require("http");
+const Todo = require("./cntr");
+const { getReqData } = require("./utils");
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(async (req, res) => {
+  if (req.url === "/api/todos" && req.method === "GET") {
+    const todos = await new Todo().getTodos();
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(todos));
+  } else if (req.url.match(/\/api\/todos\/([0-9]+)/) && req.method === "GET") {
+    try {
+      const id = req.url.split("/")[3];
+      const todo = await new Todo().getTodo(id);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(todo));
+    } catch (error) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: error }));
+    }
+  } else if (
+    req.url.match(/\/api\/todos\/([0-9]+)/) &&
+    req.method === "DELETE"
+  ) {
+    try {
+      const id = req.url.split("/")[3];
+      let message = await new Todo().deleteTodo(id);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message }));
+    } catch (error) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: error }));
+    }
+  } else if (
+    req.url.match(/\/api\/todos\/([0-9]+)/) &&
+    req.method === "PATCH"
+  ) {
+    try {
+      const id = req.url.split("/")[3];
+      let updated_todo = await new Todo().updateTodo(id);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(updated_todo));
+    } catch (error) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: error }));
+    }
+  } else if (req.url === "/api/todos" && req.method === "POST") {
+    let todo_data = await getReqData(req);
+    let todo = await new Todo().createTodo(JSON.parse(todo_data));
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(todo));
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Route not found" }));
+  }
+});
+server.listen(PORT, () => {
+  console.log(`Server working on port: ${PORT}`);
+});
