526f1b3e8a3b4e1eabeacd3121da3017eebaccac
Kiara Bici <kiara.bici16@gmail.com>
2022-11-08 15:19:51 +0100

    Program in vanilla

---
+++ data.js
@@ -0,0 +1,27 @@
+const todos = [
+  {
+    id: 1,
+    title: "HTML",
+    description: "Html courses have been completed",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "JavaScript",
+    description: "JavaScript courses have been completed",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "TypeScript",
+    description: "TypeScript courses have been completed",
+    completed: false,
+  },
+  {
+    id: 4,
+    title: "Angular",
+    description: "Angular courses have been completed",
+    completed: false,
+  },
+];
+module.exports = todos;
